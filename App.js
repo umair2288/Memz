9a87238d467e3143d26abcf82c982ed1153a7413
umair2288 <umair2288@users.noreply.github.com>
2019-05-26 08:46:10 +0530

    test change

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Mems!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>Let's create this!</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
